6495bda96f91236b5c714d55f7758554a00dfb0c
Sergii Nezdolii <sergii@frost.se>
2017-10-11 16:41:31 +0200

    [ADD] adding helper to draw car and its path on map

---
+++ MapWithCar/app/src/main/java/se/frost/mapwithcar/MapDrawHelper.kt
@@ -0,0 +1,54 @@
+package se.frost.mapwithcar
+
+import android.content.Context
+import android.support.v4.content.res.ResourcesCompat
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PolylineOptions
+
+/**
+ * MapWithCar
+ * Created by Sergii Nezdolii on 2017-10-11.
+ *
+ * Copyright (c) 2017 Frost°. All rights reserved.
+ */
+
+const val DEGREES_FULL_CIRCLE = 360.0f
+
+public class MapDrawHelper {
+
+    companion object {
+        fun drawCar(map: GoogleMap, carLocation: LatLng, title: String?, bearing: Float = 0.0f) {
+            map.addMarker(
+                    MarkerOptions()
+                            .position(carLocation)
+                            .title(title)
+                            .anchor(0.5f, 0.5f)
+                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.car_icon))
+                            .rotation(DEGREES_FULL_CIRCLE - bearing) //bearing and rotation have different directions
+            )
+        }
+
+        fun drawCarWithPath(context: Context, map: GoogleMap, locations: List<LatLng>) {
+            if (locations.isEmpty()) return
+            map.clear()
+            map.addPolyline(
+                    PolylineOptions()
+                            .addAll(locations)
+                            .color(ResourcesCompat.getColor(context.resources, R.color.orange, null))
+                            .width(5.0f)
+                            .geodesic(true)
+            )
+
+            var bearing = 0.0
+            if (locations.size >= 2) {
+                bearing = locations[locations.size - 2].bearing(locations.last())
+            }
+
+            drawCar(map, locations.last(), "Position #${locations.size}", bearing.toFloat())
+        }
+    }
+
+}
